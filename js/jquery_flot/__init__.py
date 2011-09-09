f819284bfeb3ef894a6c9494b6c3c369b5ddace8
Jan-Jaap Driessen <devnull@localhost>
2011-09-09 20:46:29 +0200

    Expose the selection functionality.

--- js/jquery_flot/__init__.py
+++ js/jquery_flot/__init__.py
@@ -10,6 +10,8 @@ def less_than_ie8_renderer(url):
 excanvas = fanstatic.Resource(library, 'excanvas.js',
     minified='excanvas.min.js', renderer=less_than_ie8_renderer)
 
-
 flot = fanstatic.Resource(library, 'jquery.flot.js',
     minified='jquery.flot.min.js', depends=[js.jquery.jquery, excanvas])
+
+selection = fanstatic.Resource(library, 'jquery.flot.selection.js',
+    minified='jquery.flot.selection.min.js', depends=[flot])
